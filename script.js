75bde797590df5c87bac232ff11f9ca55f1406a9
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-09-20 19:33:04 +0200

    ok

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Your API key: Ar37PYZFzdg4OYA9ZQZ38uY48itqTuY3NfGpK5lJlGdDsscXngjs9gSL
 
 const api = 'https://api.pexels.com/v1/search?query='
-const imgCont=document.getElementById('imgContainer')
+const imgCont = document.getElementById('imgContainer')
 
 function search() {
     const searchUser = document.getElementById('searchField').value
@@ -12,7 +12,7 @@ function search() {
         },
     })
         .then(response => response.json())
-        .then(displayUserData)
+        .then(displayUserDataConBacktick)
         .catch(error => {
             console.log(error.message)
             alert('Nessuno dato trovato')
@@ -23,12 +23,21 @@ function search() {
 function displayUserData(datiImg) {
     // console.log(datiImg)
     datiImg.photos.forEach((img) => {
-        let imgN=document.createElement('img') //creo elemento img
-        let divCol=document.createElement('div') //creo div colonna
-        imgN.classList.add('w-100','object-cover','m-2') 
-        divCol.classList.add('d-flex','col-4')
-        imgN.src=img.src.medium
+        let imgN = document.createElement('img') //creo elemento img
+        let divCol = document.createElement('div') //creo div colonna
+        imgN.classList.add('w-100', 'h-pred', 'mt-1', 'object-fit-cover')
+        divCol.classList.add('d-flex', 'col-4')
+        imgN.src = img.src.medium
         divCol.appendChild(imgN)
         imgCont.appendChild(divCol)
-    });  
+    });
+}
+
+function displayUserDataConBacktick(resultServer) {
+    imgCont.innerHTML = resultServer.photos.map((img_find) => {
+        return /*html*/`
+                <div class="col-4">
+                    <img src="${img_find.src.medium}" class="w-100 h-pred mt-1 object-fit-cover" alt="">
+                </div>`
+    }).join("")
 }
